1274df72f07fe7b087ff4aa5aa543ddd586fe4cb
Yonas <yonasseyoum24@gmail.com>
2025-09-28 00:07:32 +0300

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get("token");
+
+  if (pathname === "/auth/signin" && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (pathname === "/dashboard" && !token) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  if (pathname === "/profile" && !token) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/auth", "/dashboard"],
+};
